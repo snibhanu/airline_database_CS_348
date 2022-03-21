d87aa04c59b059b865b166e79e955670904b606f
Apoorva  Gupta <gupta481@purdue.edu>
2022-03-21 15:38:43 -0400

    Apoorva - Stage 2

--- airline_website/dat_pgs/urls.py
+++ airline_website/dat_pgs/urls.py
@@ -5,7 +5,7 @@ from . import views
 from .views import EmployeesListView, EmployeesCreateView, AirlineListView, AirlineCreateView
 #path("", homePageView, name="home")
 urlpatterns = [
-        path("<int:id>", views.index, name="index"),
+        path("passenger/<int:id>", views.index, name="index"),
         path("employees/", EmployeesListView.as_view()),
         path("add_employee/", EmployeesCreateView.as_view()),
         path("airlines/", AirlineListView.as_view()),
